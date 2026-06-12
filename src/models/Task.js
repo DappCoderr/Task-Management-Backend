6ae4e596ee25d6aa775a26690d563110c510794a
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-06-12 10:04:34 +0530

    feat: add validations into Task model

--- src/models/Task.js
+++ src/models/Task.js
@@ -12,24 +12,42 @@ export const Task = sequelize.define(
     title: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Title is required",
+        },
+        len: {
+          args: [3, 255],
+          msg: "Title must be between 3 and 255 characters",
+        },
+      },
     },
     description: {
       type: DataTypes.TEXT,
       allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: "Description is required",
+        },
+      },
     },
     status: {
       type: DataTypes.ENUM("TODO", "IN PROGRESS", "COMPLETED"),
       defaultValue: "TODO",
+      allowNull: false,
     },
     priority: {
       type: DataTypes.ENUM("LOW", "MEDIUM", "HIGH"),
       defaultValue: "LOW",
+      allowNull: false,
     },
     dueDate: {
       type: DataTypes.DATE,
+      allowNull: true,
     },
     userId: {
       type: DataTypes.UUID,
+      allowNull: false,
       references: {
         model: "users",
         key: "id",
