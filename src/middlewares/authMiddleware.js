b02b5ac1c4526cbd7491e6f460e8f59ba9e596e2
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-06-12 09:32:23 +0530

    feat: auth middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+import { verifyAccessToken } from "../utils/jwt.js";
+import StatusCodes from "http-status-codes";
+
+export const authenticate = (req, res, next) => {
+  const token = req.cookies?.accessToken;
+  if (!token) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "Access token missing" });
+  }
+  try {
+    const decoded = verifyAccessToken(token);
+    req.user = { id: decoded.sub, role: decoded.role };
+    next();
+  } catch (err) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ message: "Invalid or expired access token" });
+  }
+};
+
+export const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(StatusCodes.FORBIDDEN).json({ message: "Forbidden" });
+    }
+    next();
+  };
+};
