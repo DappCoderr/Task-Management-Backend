2960702fb35a50a9e4cc2e3447ef3a5fe4de4a09
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-06-15 17:02:01 +0530

    feat: add admin route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRoutes from "./authRoutes.js";
 import userRoutes from "./userRoutes.js";
 import taskRoutes from "./taskRoutes.js";
+import adminRoutes from "./adminRoutes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/tasks", taskRoutes);
+router.use("/admin", adminRoutes);
 
 export default router;
